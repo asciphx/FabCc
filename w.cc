ec2b4634c95fa3b0e4dc04209e3fed7254cc0303
asciphx <maxspore@foxmail.com>
2022-07-03 12:34:26 +0800

    fix

--- w.cc
+++ w.cc
@@ -1,16 +1,20 @@
 #include <lexical_cast.hh>
+#include <char_map.hh>
+#include <hpp/text.hpp>
 #include <time.h>
+#include <iostream>
+
 int main() {
-  clock_t start = clock();
-  std::string s;
+  clock_t start = clock(); cc::char_map map;
+  std::string s, num("156453", 6);
   int l;
   for (int i = 0; i < 1000000; ++i) {
 	s = std::lexical_cast<std::string>(i);
-	l = std::lexical_cast<int>("156453");
+	l = std::lexical_cast<int>(num);
   }
   printf("%.6lf\n", std::lexical_cast<double>("54345.5466"));
   //printf("%f\n", std::lexical_cast<double>("0xffffff.ff"));
   printf("use %.6f seconds\n", (float)(clock() - start) / CLOCKS_PER_SEC);
-  printf(itoa(-234).c_str());
+  text<8> t("你好世界！我好！世界"); std::cout << t;
   return 0;
 }
