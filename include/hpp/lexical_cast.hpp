7e16524b0d891341f84e121c99a1758c931e50c6
asciphx <maxspore@foxmail.com>
2022-07-03 12:24:30 +0800

    fix: inline

--- include/hpp/lexical_cast.hpp
+++ include/hpp/lexical_cast.hpp
@@ -1,3 +1,5 @@
+#ifndef LEXICAL_CAST_H
+#define LEXICAL_CAST_H
 #include <type_traits>
 #include <string>
 #include <string_view>
@@ -5,27 +7,32 @@
 #include <string.h>
 #pragma warning(disable:4244)
 namespace std {
+#if defined(_MSC_VER)
+#define _INLINE __forceinline
+#else
+#define _INLINE inline
+#endif
   static const std::string STD_TURE("true", 4), STD_FALSE("false", 5);
-  template <typename S, typename T> inline S lexical_cast(T& i) { throw std::overflow_error(""); }
-  template <> std::string lexical_cast<std::string>(signed char& i) { return std::to_string(i); }
-  template <> std::string lexical_cast<std::string>(unsigned char& i) { return std::to_string(i); }
-  template <> std::string lexical_cast<std::string>(short& i) { return std::to_string(i); }
-  template <> std::string lexical_cast<std::string>(unsigned short& i) { return std::to_string(i); }
-  template <> std::string lexical_cast<std::string>(int& i) { return std::to_string(i); }
-  template <> std::string lexical_cast<std::string>(unsigned int& i) { return std::to_string(i); }
-  template <> std::string lexical_cast<std::string>(long long& i) { return std::to_string(i); }
-  template <> std::string lexical_cast<std::string>(unsigned long long& i) { return std::to_string(i); }
-  template <> std::string lexical_cast<std::string>(bool& b) { return b ? STD_TURE : STD_FALSE; }
-  template <typename S, typename T> inline S lexical_cast(T&& i) { throw std::overflow_error(""); }
-  template <> std::string lexical_cast<std::string>(signed char&& i) { return std::to_string(i); }
-  template <> std::string lexical_cast<std::string>(unsigned char&& i) { return std::to_string(i); }
-  template <> std::string lexical_cast<std::string>(short&& i) { return std::to_string(i); }
-  template <> std::string lexical_cast<std::string>(unsigned short&& i) { return std::to_string(i); }
-  template <> std::string lexical_cast<std::string>(int&& i) { return std::to_string(i); }
-  template <> std::string lexical_cast<std::string>(unsigned int&& i) { return std::to_string(i); }
-  template <> std::string lexical_cast<std::string>(long long&& i) { return std::to_string(i); }
-  template <> std::string lexical_cast<std::string>(unsigned long long&& i) { return std::to_string(i); }
-  template <> std::string lexical_cast<std::string>(bool&& b) { return b ? STD_TURE : STD_FALSE; }
+  template <typename S, typename T> _INLINE S lexical_cast(T& i);
+  template <> _INLINE std::string lexical_cast<std::string>(signed char& i) { return std::to_string(i); }
+  template <> _INLINE std::string lexical_cast<std::string>(unsigned char& i) { return std::to_string(i); }
+  template <> _INLINE std::string lexical_cast<std::string>(short& i) { return std::to_string(i); }
+  template <> _INLINE std::string lexical_cast<std::string>(unsigned short& i) { return std::to_string(i); }
+  template <> _INLINE std::string lexical_cast<std::string>(int& i) { return std::to_string(i); }
+  template <> _INLINE std::string lexical_cast<std::string>(unsigned int& i) { return std::to_string(i); }
+  template <> _INLINE std::string lexical_cast<std::string>(long long& i) { return std::to_string(i); }
+  template <> _INLINE std::string lexical_cast<std::string>(unsigned long long& i) { return std::to_string(i); }
+  template <> _INLINE std::string lexical_cast<std::string>(bool& b) { return b ? STD_TURE : STD_FALSE; }
+  template <typename S, typename T> inline S lexical_cast(T&& i);
+  template <> _INLINE std::string lexical_cast<std::string>(signed char&& i) { return std::to_string(i); }
+  template <> _INLINE std::string lexical_cast<std::string>(unsigned char&& i) { return std::to_string(i); }
+  template <> _INLINE std::string lexical_cast<std::string>(short&& i) { return std::to_string(i); }
+  template <> _INLINE std::string lexical_cast<std::string>(unsigned short&& i) { return std::to_string(i); }
+  template <> _INLINE std::string lexical_cast<std::string>(int&& i) { return std::to_string(i); }
+  template <> _INLINE std::string lexical_cast<std::string>(unsigned int&& i) { return std::to_string(i); }
+  template <> _INLINE std::string lexical_cast<std::string>(long long&& i) { return std::to_string(i); }
+  template <> _INLINE std::string lexical_cast<std::string>(unsigned long long&& i) { return std::to_string(i); }
+  template <> _INLINE std::string lexical_cast<std::string>(bool&& b) { return b ? STD_TURE : STD_FALSE; }
   static unsigned long STD_POW[] = {
 	1,
 	10,
@@ -61,16 +68,16 @@ namespace std {
 	10000000000000000000
   };
   //const char*
-  template <typename T> T lexical_cast(const char* c) { throw std::overflow_error(""); }
-  template <> std::string lexical_cast<std::string>(const char* c) { return std::string(c); }
-  template <>
-  bool lexical_cast<bool>(const char* c) {
+  template <typename T> _INLINE T lexical_cast(const char* c);
+  template <> _INLINE std::string lexical_cast<std::string>(const char* c) { return std::string(c); }
+  template <> [[nodiscard]] _INLINE 
+   bool lexical_cast<bool>(const char* c) {
 	if ((c[0] == '1' && c[1] == 0) || (c[0] == 't' && c[1] == 'r' && c[2] == 'u' && c[3] == 'e' && c[4] == 0)) return true;
 	if ((c[0] == '0' && c[1] == 0) || (c[0] == 'f' && c[1] == 'a' && c[2] == 'l' && c[3] == 's' && c[4] == 'e' && c[5] == 0))
 	  return false; throw std::invalid_argument("");
   }
-  template <>
-  signed char lexical_cast<signed char>(const char* c) {
+  template <> [[nodiscard]] _INLINE 
+   signed char lexical_cast<signed char>(const char* c) {
 	signed char r; if (*c != 0x2D) {
 	  if (*c > 0x39 || 0x30 > *c) throw std::invalid_argument(""); r = *c - 0x30;
 	  while (*++c) {
@@ -83,8 +90,8 @@ namespace std {
 	  };
 	} return r;
   }
-  template <>
-  unsigned char lexical_cast<unsigned char>(const char* c) {
+  template <> [[nodiscard]] _INLINE 
+   unsigned char lexical_cast<unsigned char>(const char* c) {
 	if (*c > 0x39 || 0x30 > *c) throw std::invalid_argument(""); unsigned char r = *c - 0x30; char z = 0;
 	while (z != 1 && *++c) {
 	  if (*c > 0x39 || 0x30 > *c) throw std::invalid_argument(""); r = r * 10 + *c - 0x30; ++z;
@@ -95,8 +102,8 @@ namespace std {
 	  if (10 > r) throw std::out_of_range("");
 	} return r;
   }
-  template <>
-  short lexical_cast<short>(const char* c) {
+  template <> [[nodiscard]] _INLINE 
+   short lexical_cast<short>(const char* c) {
 	size_t l = strlen(c); short r = 0;
 	if (*c != 0x2D) {
 	  if (l < 6) {
@@ -118,8 +125,8 @@ namespace std {
 	  } throw std::out_of_range("");
 	}
   }
-  template <>
-  unsigned short lexical_cast<unsigned short>(const char* c) {
+  template <> [[nodiscard]] _INLINE 
+   unsigned short lexical_cast<unsigned short>(const char* c) {
 	size_t l = strlen(c); unsigned short r = 0;
 	if (l < 6) {
 	  while (l--> 1) {
@@ -130,8 +137,8 @@ namespace std {
 	  r += *c - 0x30; return r;
 	} throw std::out_of_range("");
   }
-  template <>
-  int lexical_cast<int>(const char* c) {
+  template <> [[nodiscard]] _INLINE 
+   int lexical_cast<int>(const char* c) {
 	size_t l = strlen(c); int r = 0;
 	if (*c != 0x2D) {
 	  if (l < 11) {
@@ -153,8 +160,8 @@ namespace std {
 	  } throw std::out_of_range("");
 	}
   }
-  template <>
-  unsigned int lexical_cast<unsigned int>(const char* c) {
+  template <> [[nodiscard]] _INLINE 
+   unsigned int lexical_cast<unsigned int>(const char* c) {
 	size_t l = strlen(c); unsigned int r = 0;
 	if (l < 11) {
 	  while (l--> 1) {
@@ -165,8 +172,8 @@ namespace std {
 	  r += *c - 0x30; return r;
 	} throw std::out_of_range("");
   }
-  template <>
-  long long lexical_cast<long long>(const char* c) {
+  template <> [[nodiscard]] _INLINE 
+   long long lexical_cast<long long>(const char* c) {
 	size_t l = strlen(c); long long r = 0;
 	if (*c != 0x2D) {
 	  if (l < 20) {
@@ -188,8 +195,8 @@ namespace std {
 	  } throw std::out_of_range("");
 	}
   }
-  template <>
-  unsigned long long lexical_cast<unsigned long long>(const char* c) {
+  template <> [[nodiscard]] _INLINE 
+   unsigned long long lexical_cast<unsigned long long>(const char* c) {
 	size_t l = strlen(c); unsigned long long r = 0;
 	if (l < 21) {
 	  while (l--> 1) {
@@ -200,20 +207,20 @@ namespace std {
 	  r += *c - 0x30; return r;
 	} throw std::out_of_range("");
   }
-  template <>
-  float lexical_cast<float>(const char* c) {
+  template <> [[nodiscard]] _INLINE 
+   float lexical_cast<float>(const char* c) {
 	char* $; const float _ = ::strtof(c, &$); if (*$ == 0) return _; throw std::invalid_argument("");
   }
-  template <>
-  double lexical_cast<double>(const char* c) {
+  template <> [[nodiscard]] _INLINE 
+   double lexical_cast<double>(const char* c) {
 	char* $; const double _ = ::strtod(c, &$); if (*$ == 0) return _; throw std::invalid_argument("");
   }
-  template <>
-  long double lexical_cast<long double>(const char* c) {
+  template <> [[nodiscard]] _INLINE 
+   long double lexical_cast<long double>(const char* c) {
 	char* $; const long double _ = ::strtold(c, &$); if (*$ == 0) return _; throw std::invalid_argument("");
   }
-  template <>
-  tm lexical_cast<tm>(const char* c) {
+  template <> _INLINE 
+   tm lexical_cast<tm>(const char* c) {
 	int year = 0, month = 0, day = 0, hour = 0, min = 0, sec = 0; tm t;
 	if (sscanf(c, "%4d-%2d-%2d %2d:%2d:%2d", &year, &month, &day, &hour, &min, &sec) == 6) {
 	  t.tm_mday = day;
@@ -226,15 +233,13 @@ namespace std {
 	return t;
   }
   //std::string
-  template <typename T> T lexical_cast(std::string& s) {
-	static_assert(std::is_convertible<std::string, T>::value, "Impossible to convert T"); throw std::overflow_error("");
-  }
-  template <>
-  bool lexical_cast<bool>(std::string& s) {
+  template <typename T> _INLINE T lexical_cast(std::string& s);
+  template <> [[nodiscard]] _INLINE 
+   bool lexical_cast<bool>(std::string& s) {
 	if (s == "1" || s == "true")return true; if (s == "0" || s == "false")return false; throw std::invalid_argument("");
   }
-  template <>
-  signed char lexical_cast<signed char>(std::string& s) {
+  template <> [[nodiscard]] _INLINE 
+   signed char lexical_cast<signed char>(std::string& s) {
 	const char* c = s.c_str(); size_t l = s.size(); signed char r = 0;
 	if (*c != 0x2D) {
 	  if (l < 4) {
@@ -256,8 +261,8 @@ namespace std {
 	  } throw std::out_of_range("");
 	}
   }
-  template <>
-  unsigned char lexical_cast<unsigned char>(std::string& s) {
+  template <> [[nodiscard]] _INLINE 
+   unsigned char lexical_cast<unsigned char>(std::string& s) {
 	const char* c = s.c_str(); size_t l = s.size(); unsigned char r = 0;
 	if (l < 4) {
 	  while (l--> 1) {
@@ -268,8 +273,8 @@ namespace std {
 	  r += *c - 0x30; return r;
 	} throw std::out_of_range("");
   }
-  template <>
-  short lexical_cast<short>(std::string& s) {
+  template <> [[nodiscard]] _INLINE 
+   short lexical_cast<short>(std::string& s) {
 	const char* c = s.c_str(); size_t l = s.size(); short r = 0;
 	if (*c != 0x2D) {
 	  if (l < 6) {
@@ -291,8 +296,8 @@ namespace std {
 	  } throw std::out_of_range("");
 	}
   }
-  template <>
-  unsigned short lexical_cast<unsigned short>(std::string& s) {
+  template <> [[nodiscard]] _INLINE 
+   unsigned short lexical_cast<unsigned short>(std::string& s) {
 	const char* c = s.c_str(); size_t l = s.size(); unsigned short r = 0;
 	if (l < 6) {
 	  while (l--> 1) {
@@ -303,8 +308,8 @@ namespace std {
 	  r += *c - 0x30; return r;
 	} throw std::out_of_range("");
   }
-  template <>
-  int lexical_cast<int>(std::string& s) {
+  template <> [[nodiscard]] _INLINE 
+   int lexical_cast<int>(std::string& s) {
 	const char* c = s.c_str(); size_t l = s.size(); int r = 0;
 	if (*c != 0x2D) {
 	  if (l < 11) {
@@ -326,8 +331,8 @@ namespace std {
 	  } throw std::out_of_range("");
 	}
   }
-  template <>
-  unsigned int lexical_cast<unsigned int>(std::string& s) {
+  template <> [[nodiscard]] _INLINE 
+   unsigned int lexical_cast<unsigned int>(std::string& s) {
 	const char* c = s.c_str(); size_t l = s.size(); unsigned int r = 0;
 	if (l < 11) {
 	  while (l--> 1) {
@@ -338,8 +343,8 @@ namespace std {
 	  r += *c - 0x30; return r;
 	} throw std::out_of_range("");
   }
-  template <>
-  long long lexical_cast<long long>(std::string& s) {
+  template <> [[nodiscard]] _INLINE 
+   long long lexical_cast<long long>(std::string& s) {
 	const char* c = s.c_str(); size_t l = s.size(); long long r = 0;
 	if (*c != 0x2D) {
 	  if (l < 20) {
@@ -361,8 +366,8 @@ namespace std {
 	  } throw std::out_of_range("");
 	}
   }
-  template <>
-  unsigned long long lexical_cast<unsigned long long>(std::string& s) {
+  template <> [[nodiscard]] _INLINE 
+   unsigned long long lexical_cast<unsigned long long>(std::string& s) {
 	const char* c = s.c_str(); size_t l = s.size(); unsigned long long r = 0;
 	if (l < 21) {
 	  while (l--> 1) {
@@ -373,23 +378,23 @@ namespace std {
 	  r += *c - 0x30; return r;
 	} throw std::out_of_range("");
   }
-  template <>
-  float lexical_cast<float>(std::string& s) {
+  template <> [[nodiscard]] _INLINE 
+   float lexical_cast<float>(std::string& s) {
 	const char* c = s.c_str();
 	char* $; const float _ = ::strtof(c, &$); if (*$ == 0) return _; throw std::invalid_argument("");
   }
-  template <>
-  double lexical_cast<double>(std::string& s) {
+  template <> [[nodiscard]] _INLINE 
+   double lexical_cast<double>(std::string& s) {
 	const char* c = s.c_str();
 	char* $; const double _ = ::strtod(c, &$); if (*$ == 0) return _; throw std::invalid_argument("");
   }
-  template <>
-  long double lexical_cast<long double>(std::string& s) {
+  template <> [[nodiscard]] _INLINE 
+   long double lexical_cast<long double>(std::string& s) {
 	const char* c = s.c_str();
 	char* $; const long double _ = ::strtold(c, &$); if (*$ == 0) return _; throw std::invalid_argument("");
   }
-  template <>
-  tm lexical_cast<tm>(std::string& s) {
+  template <> _INLINE 
+   tm lexical_cast<tm>(std::string& s) {
 	const char* c = s.c_str();
 	int year = 0, month = 0, day = 0, hour = 0, min = 0, sec = 0; tm t;
 	if (sscanf(c, "%4d-%2d-%2d %2d:%2d:%2d", &year, &month, &day, &hour, &min, &sec) == 6) {
@@ -402,4 +407,6 @@ namespace std {
 	t.tm_mon = month - 1;
 	return t;
   }
+#undef _INLINE
 }
+#endif // LEXICAL_CAST_H
