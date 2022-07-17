1768b500c5b7c028f882c13e8ea130b0011b2e56
asciphx <maxspore@foxmail.com>
2022-07-17 22:07:42 +0800

    sup: RFC3986

---
+++ fc/include/directory.hh
@@ -0,0 +1,102 @@
+#pragma once
+#include <stdlib.h>
+#include <memory>
+#include <string>
+#include <req-res.hh>
+#include <hpp/http_error.hpp>
+
+namespace fc {
+namespace impl {
+inline bool is_regular_file(const std::string& path) {
+  struct stat path_stat;
+  if (-1 == stat(path.c_str(), &path_stat))
+    return false;
+  return path_stat.st_mode & S_IFREG;
+}
+inline bool is_directory(const std::string& path) {
+  struct stat path_stat;
+  if (-1 == stat(path.c_str(), &path_stat))
+    return false;
+  return path_stat.st_mode & S_IFDIR;
+}
+
+inline bool starts_with(const char* pre, const char* str) {
+  size_t lenpre = strlen(pre), lenstr = strlen(str);
+  return lenstr < lenpre ? false : memcmp(pre, str, lenpre) == 0;
+}
+} // namespace impl
+
+#if defined _WIN32
+#define CROSSPLATFORM_MAX_PATH MAX_PATH
+#else
+#define CROSSPLATFORM_MAX_PATH PATH_MAX
+#endif
+template <unsigned S> bool crossplatform_realpath(const std::string& path, char out_buffer[S]) {
+  // Check if file exists by real file path.
+#if defined _WIN32
+  return 0 != GetFullPathNameA(path.c_str(), S, out_buffer, nullptr);
+#else
+  return nullptr != realpath(path.c_str(), out_buffer);
+#endif
+}
+inline auto serve_file(const std::string& root, std::string_view path, Res& response) {
+  static char dot = '.', slash = '/';
+  // remove first slash if needed.
+  if (!path.empty() && path[0] == slash) {
+    path = std::string_view(path.data() + 1, path.size() - 1); // erase(0, 1);
+  }
+  if (path[0] == ' ') path = "index.html";
+  // Directory listing not supported.
+  std::string full_path(root + std::string(path));
+  //std::cout << path << " + " << full_path << std::endl;
+  if (path.empty() || !impl::is_regular_file(full_path)) {
+    throw http_error::not_found("file not found.");
+  }
+  // Check if file exists by real file path.
+  char realpath_out[CROSSPLATFORM_MAX_PATH]{0};
+  if (!crossplatform_realpath<CROSSPLATFORM_MAX_PATH>(full_path, realpath_out))
+    throw http_error::not_found("file not found.");
+  
+  //std::cout << root.c_str() << " + " << realpath_out << std::endl;
+  // Check that path is within the root directory.
+  if (!impl::starts_with(root.c_str(), realpath_out))
+    throw http_error::not_found("Access denied.");
+
+  response.write_file(full_path);
+};
+
+inline auto serve_directory(const std::string& root) {
+  // extract root realpath.
+  char realpath_out[CROSSPLATFORM_MAX_PATH]{0};
+  if (!crossplatform_realpath<CROSSPLATFORM_MAX_PATH>(root, realpath_out))
+    throw std::runtime_error(std::string("serve_directory error: Directory ") + root +
+                             " does not exists.");
+  // Check if it is a directory.
+  if (!impl::is_directory(realpath_out)) {
+    throw std::runtime_error(std::string("serve_directory error: ") + root +
+                             " is not a directory.");
+  }
+  // Ensure the root ends with a /
+  std::string real_root(realpath_out);
+#if _WIN32
+  if (real_root.back() != '\\') {
+    real_root.push_back('\\');
+  }
+#elif
+  if (real_root.back() != '/') {
+    real_root.push_back('/');
+  }
+#endif
+  App api;
+  api.get("/{{path...}}") = [real_root](Req& request, Res& response) {
+    return serve_file(real_root, path, response);
+  };
+  return api;
+    // subapi.routes_map_.for_all_routes([this, prefix](auto r, VH h) {
+    //   if (!r.empty() && r.back() == '/') h.url_spec = prefix + r;
+    //   else h.url_spec = prefix + '/' + r;
+    //   this->routes_map_[h.url_spec] = h;
+    // });
+}
+
+} // namespace li
--- test/UrlResolution.cc
+++ test/UrlResolution.cc
@@ -10,17 +10,40 @@ std::string DecodeURL(const char* d) {
   const char* e = c + s.length(); while (c < e) {
 	if (*c == '%' && c < e - 2) {
 	  *o = (_X[c[1]] << 4) | _X[c[2]]; c += 2;
+	} else if (*c == '+') {
+	  *o = ' ';
 	} else if (o != c) *o = *c; ++o; ++c;
   } return std::string(s.c_str(), o - s.c_str());// 0x67
 }
+std::string DecodeURL(std::string& s) {
+  char* o = (char*)s.c_str(), * c = (char*)s.c_str();
+  const char* e = c + s.size(); while (c < e) {
+	if (*c == '%' && c < e - 2) {
+	  *o = (_X[c[1]] << 4) | _X[c[2]]; c += 2;
+	} else if (*c == '+') {
+	  *o = ' ';
+	} else if (o != c) *o = *c; ++o; ++c;
+  } return std::string(s.data(), o - s.data());
+}
+//static const char _RFC2396[] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0,
+//0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1,
+//0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 0 };
+//static const char _RFC3986[] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0,
+//1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1,
+//0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 0 };
 static const char _H[] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0,
-0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1,
-0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 0 };
+0, 0, 0, 0, 0, 0, 0x2b, 0x21, 0, 0x23, 0x24, 0, 0x26, 0x27, 0x28, 0x29, 0x2a, 0, 0x2c, 0x2d , 0x2e, 0x2f,
+0x30 , 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39, 0x3a, 0x3b, 0, 0x3d, 0, 0x3f, 0x40, 0x41,
+0x42, 0x43, 0x44, 0x45, 0x46, 0x47, 0x48, 0x49, 0x4a, 0x4b, 0x4c, 0x4d, 0x4e, 0x4f, 0x50, 0x51, 0x52,
+0x53, 0x54, 0x55, 0x56, 0x57, 0x58, 0x59, 0x5a, 0x5b, 0, 0x5d, 0, 0x5f, 0, 0x61, 0x62, 0x63, 0x64,
+0x65, 0x66, 0x67, 0x68, 0x69, 0x6a, 0x6b, 0x6c, 0x6d, 0x6e, 0x6f, 0x70, 0x71, 0x72, 0x73, 0x74, 0x75,
+0x76, 0x77, 0x78, 0x79, 0x7a, 0, 0, 0, 0x7e, 0 };//RFC3986
 std::string EncodeURL(const std::string& s) {
   std::string r; for (char c : s) {
 	if (c > '\377') {
-	  if (_H[c])r.push_back(c);
+	  if (_H[c])r.push_back(_H[c]);//if (_RFC2396[c])r.push_back(c);
 	  else {
 		r.push_back(0x25); char o = (c & 0xF0) >> 4;
 		o += o > 9 ? 0x37 : 0x30; r.push_back(o);
@@ -37,7 +60,7 @@ std::string EncodeURL(const std::string& s) {
 std::string EncodeURL(const char* c) {
   std::string r; while (*c) {
 	if (*c > '\377') {
-	  if (_H[*c])r.push_back(*c);
+	  if (_H[*c])r.push_back(_H[*c]);
 	  else {
 		r.push_back(0x25); char o = (*c & 0xF0) >> 4;
 		o += o > 9 ? 0x37 : 0x30; r.push_back(o);
@@ -52,14 +75,14 @@ std::string EncodeURL(const char* c) {
 	} ++c;
   } return r;
 }
-const char* ccc = "http%3A%2F%2Fcontoso.sharepoint.com%2FFinance%2FProfit%20and%20Loss%20Statement.xlsx";
+const char* old = "https://github.com/search?l=C++&q=http server&type=Repositories";
+const char* ccc = "https://github.com/search?l=C%2B%2B&q=http+server&type=Repositories";
 int main() {
-  std::string xlsx = "http%3A%2F%2Fcontoso.sharepoint.com%2FFinance%2FProfit";
-  std::string ss = DecodeURL(ccc);
+  std::string ss = EncodeURL(old);
   std::cout << ss << std::endl;
-  ss = EncodeURL(ss);
-  std::cout << ss << std::endl;
-  std::cout << ccc << std::endl;
   assert(ccc == ss);
+  ss = DecodeURL(ss);
+  std::cout << ss << std::endl;
+  assert(old == ss);
   return 0;
 }
